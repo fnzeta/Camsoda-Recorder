066666e59dddc14b5667daa4d50f9d94713c64ac
fnzeta <46158711+fnzeta@users.noreply.github.com>
2019-08-03 18:35:46 +0530

    initial impl

---
+++ classes/helper.py
@@ -0,0 +1,13 @@
+
+
+class url:
+    online_api = 'https://www.camsoda.com/api/v1/browse/online'
+    video_api = 'https://www.camsoda.com/api/v1/video/vtoken/{model}?username=guest_{num}'
+    stream = 'https://{server}/cam/mp4:{stream_name}_h264_aac_480p/playlist.m3u8?token={token}'
+    user = 'https://www.camsoda.com/api/v1/user/{model}'
+
+
+class rec_command:
+    ffmpeg = 'ffmpeg -i {stream} -c copy {dl_path}'
+    livestreamer = 'livestreamer --yes-run-as-root hlsvariant://{stream} best -f -o {dl_path}'
+    streamlink = 'streamlink hlsvariant://{stream} best -o {dl_path}'
---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import datetime
+import time
+from classes.camsoda import get, Config, Wanted, should_record_model, start_recording, RecordingThread
+
+if __name__ == '__main__':
+    r = get().requests_session()
+    conf = Config(os.path.join(sys.path[0], 'config.conf'))
+    Wanted = Wanted(conf.settings)
+    next_run = datetime.datetime.now()
+    while True:
+        if datetime.datetime.now() < next_run:
+            time.sleep(0.1)
+            continue
+        next_run += datetime.timedelta(seconds=conf.settings.interval)
+        print("checking for online models")
+        next_run += datetime.timedelta(seconds=conf.settings.interval)
+        conf.refresh()
+        online = get().online_models(r)
+        if online:
+            for model in online:
+                if should_record_model(conf.settings).check(model):
+                    start_recording(model, conf, r)
+        else:
+            next_run = datetime.datetime.now()
+        print('finished check, next check in {} seconds'.format(
+            conf.settings.interval))
+        print('currently recording {} models: {}'.format(len(RecordingThread.currently_recording_models.keys(
+        )), list(RecordingThread.currently_recording_models.keys())))
